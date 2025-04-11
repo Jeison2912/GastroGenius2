295d8fad10fa779ae0c0cd244d34d099f1cdb7a4
Jeison2912 <jeison29121999@gmail.com>
2025-04-10 20:38:06 -0500

    Se agrego parte vertical del de la interfaz de home

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation("com.makeramen:roundedimageview:2.3.0") // ✅ Corrección aquí
+    implementation("com.makeramen:roundedimageview:2.3.0")
 }
